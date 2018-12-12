80dc6438164e1bf1c60acc0e3ddbed26a6827dc9
YusofBandar <ybandar1902@gmail.com>
2018-12-12 16:18:49 +0000

    AngularJS init

---
+++ spwa/src/app/app.js
@@ -0,0 +1,43 @@
+angular.module('app', ['ui.router']);
+
+
+angular.module('app').config(config);
+
+config.$inject = [
+    '$stateProvider',
+    '$urlRouterProvider'
+]
+
+function config($stateProvider, $urlRouterProvider) {
+
+    /* 
+     Index Page
+     user can enter ip address and channel number
+    */
+    var indexState = {
+        name: 'index',
+        url: '/index',
+        templateUrl: 'app/shared/index/indexView.html',
+        controller: 'IndexViewCtrl',
+        controllerAs: 'indexView'
+    }
+
+  
+    $stateProvider.state(indexState);
+
+
+    $urlRouterProvider.otherwise('/index');
+
+  
+}
+
+angular.module('app').run(run);
+
+run.$inject = [
+    '$rootScope'
+]
+
+function run($rootScope) {
+    console.log('version 1.0.0');
+    $rootScope.defaultUrl = '192.168.1.102';
+}
---
+++ spwa/src/app/shared/index/IndexViewCtrl.js
@@ -0,0 +1,9 @@
+angular.module('app').controller('IndexViewCtrl', IndexViewCtrl);
+
+IndexViewCtrl.$inject = [
+ 
+];
+
+function IndexViewCtrl() {
+   
+}
