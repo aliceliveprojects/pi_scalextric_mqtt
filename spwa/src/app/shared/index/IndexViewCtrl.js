93015f7b847ca834ae70fe6bfe3da04c252dda1f
YusofBandar <ybandar1902@gmail.com>
2018-12-12 17:46:35 +0000

    Reworked form to have a subscribe and publish message

--- spwa/src/app/shared/index/IndexViewCtrl.js
+++ spwa/src/app/shared/index/IndexViewCtrl.js
@@ -5,45 +5,49 @@ IndexViewCtrl.$inject = [
     'mqttService'
 ];
 
-function IndexViewCtrl($rootScope,mqttService) {
+function IndexViewCtrl($rootScope, mqttService) {
     var vm = this;
 
     vm.ip_address = $rootScope.defaultUrl;
     vm.mqtt_topic = "";
     vm.mqtt_message = "";
-    vm.logs = ["hello"];
-    vm.go = go;
+    vm.logs = [];
+    vm.subscribe = subscribe;
+    vm.publish = publish;
 
-    function go(valid) {
+    function subscribe(valid) {
         if (!valid) {
             alert("Invalid Details")
         } else {
-           console.log("valid");
-          
-            
-        }
 
+        }
+    }
+
+    function publish(valid) {
+        if (!valid || !vm.mqtt_message) {
+            alert("Invalid Details")
+        } else {
+
+        }
     }
 
     activate();
-
-
-    function activate(){
+    function activate() {
         console.log("connecting to mqtt");
         //connection only works with broker.hivemq.com
         //taken from https://github.com/mqtt/mqtt.github.io/wiki/public_brokers
-        mqttService.initialize("broker.hivemq.com",8000);
-        mqttService.onConnectionLost(function(){
+        mqttService.initialize("broker.hivemq.com", 8000);
+        mqttService.onConnectionLost(function () {
             console.log("connection lost");
         });
-        mqttService.onMessageArrived(function(message){
-            console.log("message ",message.payloadString);
+        mqttService.onMessageArrived(function (message) {
+            console.log("message ", message.payloadString);
         });
-        mqttService.connect(function(){
+        mqttService.connect(function () {
             console.log("connected");
-            mqttService.subscribe("hello");
-            //mqttService.publish("hello","hello from mqtt");
+            mqttService.subscribe("pi_scalextrix_mqtt");
+
         });
-       
+
     }
 }
