359ea63b9434104bb7f3bbb60fefc34589a4f0c9
YusofBandar <ybandar1902@gmail.com>
2018-12-12 16:25:44 +0000

    Completed MQTT form

--- spwa/src/app/shared/index/IndexViewCtrl.js
+++ spwa/src/app/shared/index/IndexViewCtrl.js
@@ -1,9 +1,25 @@
 angular.module('app').controller('IndexViewCtrl', IndexViewCtrl);
 
 IndexViewCtrl.$inject = [
- 
+    '$rootScope',
 ];
 
-function IndexViewCtrl() {
-   
+function IndexViewCtrl($rootScope) {
+    var vm = this;
+
+    vm.ip_address = $rootScope.defaultUrl;
+    vm.mqtt_topic = "";
+    vm.mqtt_message = "";
+    vm.go = go;
+
+    function go(valid) {
+        if (!valid) {
+            alert("Invalid Details")
+        } else {
+           console.log("valid");
+          
+            
+        }
+
+    }
 }
