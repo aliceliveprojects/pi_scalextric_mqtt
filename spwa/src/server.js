8af2da60c13a1a7479a779382539788fd1c723e3
Yusof Bandar <ybandar1902@gmail.com>
2018-12-13 15:46:53 +0000

    Add Cors To Server

--- spwa/src/server.js
+++ spwa/src/server.js
@@ -7,6 +7,13 @@ const app = express();
 
 app.use(express.static(path.join(__dirname, '')));
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+
 app.get('/*', (req, res) => {
   res.sendFile(path.join(__dirname + '/index.html'));
 });
