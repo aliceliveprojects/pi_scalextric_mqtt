a5225f7166df0d088a27403938cf57299b2d1e9d
YusofBandar <ybandar1902@gmail.com>
2019-01-03 13:41:02 +0000

    Removed Unused Root Scope Vars

--- spwa/src/app/app.js
+++ spwa/src/app/app.js
@@ -34,8 +34,6 @@ run.$inject = [
     '$rootScope'
 ]
 
-function run($rootScope) {
+function run() {
     console.log('version 1.0.0 Yusof Bandar');
-    $rootScope.defaultUrl = '192.168.1.3';
-    $rootScope.defaultPort = '9001';
 }
