5b73d70a4d7331c053f890ff37fb546332a33f6f
YusofBandar <ybandar1902@gmail.com>
2019-01-03 16:26:57 +0000

    Pi Scalextric Service Init
    - this service will be used to control to communicate with node red via mqtt

---
+++ spwa/src/app/components/piScalextric/piScalextricService.js
@@ -0,0 +1,11 @@
+angular.module('app').service('piScalextricService',piScalextricService);
+
+piScalextricService.$inject = [
+];
+
+
+function piScalextricService() {
+    var self = this;
+
+    
+}
