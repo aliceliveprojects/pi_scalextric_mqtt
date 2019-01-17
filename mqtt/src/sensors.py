cdcb87bdae698e2b4a97199dd62042348c1e3ad0
YusofBandar <ybandar1902@gmail.com>
2019-01-17 12:50:13 +0000

    Started Sensor Broker Test

---
+++ mqtt/src/sensors.py
@@ -0,0 +1,62 @@
+import argparse
+import json
+import time
+import paho.mqtt.client as mqttClient
+
+
+
+
+def on_connect(client, userdata, flags, rc):
+    if(rc == 0):
+        print("connected to broker")
+        global Connected
+        Connected = True
+        
+        client.message_callback_add("testUUID/sensors",sensorDetails)
+        client.subscribe("testUUID/sensors")
+
+def sensorDetails(client, userdata, message):
+
+    print("Received message '" + str(message.payload) + "' on topic '"+ message.topic + "' with QoS " + str(message.qos))
+    
+
+
+# Define Command Line Arguments
+parser = argparse.ArgumentParser(description='Publishes to Sensor Event once a sensor is above the specified threshold')
+parser.add_argument('piUUID',help='UUID of the pi')
+parser.add_argument('broker_address', help='Broker address, not including port example: m15.cloudmqtt.com')
+parser.add_argument('broker_port',help='Port for broker',type=int)
+parser.add_argument('username',help='Username for broker, if username is not being used pass -1')
+parser.add_argument('password',help='Password for broker, if password is not being used pass -1')
+parser.add_argument('sensorFilePath', help='Absolute path to the sensor.json file')
+
+args = parser.parse_args()
+
+
+Connected = False
+
+# Broker Details Taken From Command Line Args
+piID = args.piUUID
+broker_address = args.broker_address
+port = args.broker_port
+username = args.username
+password = args.password
+
+# Setup Client	
+client = mqttClient.Client(client_id="",clean_session=True)
+
+# Setup Client Username And Password if Specified
+if username != '-1':
+    if password != '-1':
+        client.username_pw_set(username, password=password)
+    else:
+        client.username_pw_set(username)
+
+
+client.on_connect= on_connect
+
+
+
+# Establish Connection To Broker
+client.connect(broker_address, port=port)
+client.loop_forever()
