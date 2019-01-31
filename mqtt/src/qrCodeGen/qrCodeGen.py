28f9a046935cb8dbdeea3a44747b44f2bae1dc88
YusofBandar <ybandar1902@gmail.com>
2019-01-31 14:27:04 +0000

    Added Main Function To Qr Code Gen

--- mqtt/src/qrCodeGen/qrCodeGen.py
+++ mqtt/src/qrCodeGen/qrCodeGen.py
@@ -13,58 +13,63 @@ parser.add_argument('--filename',nargs='?',help='file name of the qr code image'
 
 args = parser.parse_args()
 
-
-DEFAULTFILENAME = 'QrCode'
-host = args.host
-config = None
-
-
-# Read config details from config.json file
-with open(args.config) as json_data:
-    config = json.load(json_data)
-    
-# Check config contains the correct details
-if('uuid' not in config):
-    raise KeyError('uuid was not found in config')
-
-if('host' not in config['broker']):
-    raise KeyError('broker host was not found in config')
-
-if('port' not in config['broker']):
-    raise KeyError('boker port was not found in config')
+def main():
+    DEFAULTFILENAME = 'QrCode'
+    host = args.host
+    config = None
 
 
-urlParams = config['broker']
-urlParams['uuid'] = config['uuid']
+    # Read config details from config.json file
+    with open(args.config) as json_data:
+        config = json.load(json_data)
+        
+    # Check config contains the correct details
+    if('uuid' not in config):
+        raise KeyError('uuid was not found in config')
 
-# Rename keys for url paramaters
-urlParams['brokerHost'] = urlParams.pop('host')
-urlParams['brokerPort'] = urlParams.pop('port')
+    if('host' not in config['broker']):
+        raise KeyError('broker host was not found in config')
 
-# Params url encoded
-encodedUrlParams = urllib.urlencode(urlParams)
-
-# Final Url 
-url = "{}?{}".format(host,encodedUrlParams)
+    if('port' not in config['broker']):
+        raise KeyError('boker port was not found in config')
 
 
+    urlParams = config['broker']
+    urlParams['uuid'] = config['uuid']
 
-# Generate Qr Code
-qr = qrcode.QRCode(
-    version= 5,
-    error_correction=qrcode.constants.ERROR_CORRECT_L,
-    box_size= 10,
-    border=4,
-)
+    # Rename keys for url paramaters
+    urlParams['brokerHost'] = urlParams.pop('host')
+    urlParams['brokerPort'] = urlParams.pop('port')
 
-qr.add_data(url)
-qr.make(fit=True)
-img = qr.make_image(fill_color="black",back_colour="white")
+    # Params url encoded
+    encodedUrlParams = urllib.urlencode(urlParams)
 
-# Save Qr Code Image
-fileName = "{}.{}".format(DEFAULTFILENAME,'png')
-if(args.filename != None):
-    fileName = args.filename
+    # Final Url 
+    url = "{}?{}".format(host,encodedUrlParams)
 
-img.save(fileName)
+
+
+    # Generate Qr Code
+    qr = qrcode.QRCode(
+        version= 5,
+        error_correction=qrcode.constants.ERROR_CORRECT_L,
+        box_size= 10,
+        border=4,
+    )
+
+    qr.add_data(url)
+    qr.make(fit=True)
+    img = qr.make_image(fill_color="black",back_colour="white")
+
+    # Save Qr Code Image
+    fileName = "{}.{}".format(DEFAULTFILENAME,'png')
+    if(args.filename != None):
+        fileName = args.filename
+
+    img.save(fileName)
+
+
+if __name__=='__main__':
+    main()
+
 
