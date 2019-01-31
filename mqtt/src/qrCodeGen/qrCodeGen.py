b7ba559453177f6daba4acc703b55389e6822849
YusofBandar <ybandar1902@gmail.com>
2019-01-31 14:24:03 +0000

    Added Command Line Args To Qr Code Gen

--- mqtt/src/qrCodeGen/qrCodeGen.py
+++ mqtt/src/qrCodeGen/qrCodeGen.py
@@ -2,15 +2,25 @@ import qrcode
 import qrcode.image.svg
 import json
 import urllib
+import argparse
 
-host = "https://aliceliveprojects.github.io/pi_scalextric_mqtt/spwa/src/index.html#!/index"
+
+# Define Command Line Arguments
+parser = argparse.ArgumentParser(description='Generates url qr code containing single page web app domain and broker details')
+parser.add_argument('config',help='path to the config.json file')
+parser.add_argument('host',help='host of the single page web app')
+parser.add_argument('--filename',nargs='?',help='file name of the qr code image')
+
+args = parser.parse_args()
 
 
+DEFAULTFILENAME = 'QrCode'
+host = args.host
 config = None
 
 
 # Read config details from config.json file
-with open('../config.json') as json_data:
+with open(args.config) as json_data:
     config = json.load(json_data)
     
 # Check config contains the correct details
@@ -31,22 +41,30 @@ urlParams['uuid'] = config['uuid']
 urlParams['brokerHost'] = urlParams.pop('host')
 urlParams['brokerPort'] = urlParams.pop('port')
 
+# Params url encoded
 encodedUrlParams = urllib.urlencode(urlParams)
 
+# Final Url 
 url = "{}?{}".format(host,encodedUrlParams)
 
-factory = qrcode.image.svg.SvgImage
 
+
+# Generate Qr Code
 qr = qrcode.QRCode(
     version= 5,
     error_correction=qrcode.constants.ERROR_CORRECT_L,
     box_size= 10,
-    border=4
+    border=4,
 )
 
 qr.add_data(url)
 qr.make(fit=True)
-
 img = qr.make_image(fill_color="black",back_colour="white")
-img.save("image.jpg")
+
+# Save Qr Code Image
+fileName = "{}.{}".format(DEFAULTFILENAME,'png')
+if(args.filename != None):
+    fileName = args.filename
+
+img.save(fileName)
 
